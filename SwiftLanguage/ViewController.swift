786dfaaa951d8f6ee1f1a4476b147e1b0a4f6805
刘晓亮(平安万家) <liuxiaoliang606@c02s3d77g8wn.local>
2017-04-14 10:36:56 +0800

    Initial Commit

---
+++ SwiftLanguage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLanguage
+//
+//  Created by 刘晓亮(平安万家) on 17/4/14.
+//  Copyright © 2017年 pingan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
